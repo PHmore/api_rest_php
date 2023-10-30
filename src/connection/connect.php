3da0348f24b8626ba63820f1baaa93374c83b716
Patryck Henryck <patryckhenryck401@gmail.com>
2023-10-30 16:57:31 -0300

    feat (all): Criação arquivos base

---
+++ src/connection/connect.php
@@ -0,0 +1,13 @@
+<?php 
+//Classe que será usada para instanciar conexão com banco de dados
+class Connection{
+    public static function Connect(){
+        $host = "localhost";
+        $dbname = "api_php";//Insira aqui nome do banco de dados 
+        $user = "root"; //Isira aqui seu nome de usuário;
+        $password = null; //Insira aqui sua senha;
+        
+        return new PDO("mysql:host={$host};dbname={$dbname};charset=UTF8;",$user, $password);
+    }
+}
+?>
